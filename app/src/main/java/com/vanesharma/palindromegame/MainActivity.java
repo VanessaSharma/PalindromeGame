baab02c84842f3888b38cfdeb00b5727ddedbb41
Vanessa Palacios Sharma <vanelunasharma@gmail.com>
2017-04-16 10:33:08 -0700

    Add ButterKnife library.

--- app/src/main/java/com/vanesharma/palindromegame/MainActivity.java
+++ app/src/main/java/com/vanesharma/palindromegame/MainActivity.java
@@ -2,12 +2,20 @@ package com.vanesharma.palindromegame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.titleTextView) TextView mTitleTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
