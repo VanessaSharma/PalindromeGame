49db4b986e0810d94b34035bf2e29d56685960db
Vanessa Palacios Sharma <vanelunasharma@gmail.com>
2017-04-16 10:57:15 -0700

    Add test for MainActivity titleTextView.

---
+++ app/src/test/java/com/vanesharma/palindromegame/MainActivityTest.java
@@ -0,0 +1,33 @@
+package com.vanesharma.palindromegame;
+
+
+import android.os.Build;
+import android.widget.TextView;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+import android.widget.TextView;
+import static junit.framework.Assert.assertTrue;
+
+
+@Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP)
+@RunWith(RobolectricGradleTestRunner.class)
+
+public class MainActivityTest {
+    private MainActivity activity;
+
+    @Before
+    public void setup(){
+        activity = Robolectric.setupActivity(MainActivity.class);
+    }
+
+    @Test
+    public void validateTextViewContent(){
+        TextView titleTextView = (TextView) activity.findViewById(R.id.titleTextView);
+        assertTrue("Palindrome".equals(titleTextView.getText().toString()));
+    }
+}
